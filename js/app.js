e1ab475ce24c5db7bce4de316c5d637fd70c8963
Kevin Burdett <kevin@kevinburdett.com>
2014-03-30 01:08:41 -0400

    Update httpProvider config for CORS support.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,17 @@ homepageApp.config([
         $routeProvider.when('/about_me', { templateUrl: 'partials/about_me.html', title: 'About Me' }).when('/about_page', { templateUrl: 'partials/about_page.html', title: 'About This Page' }).when('/aiken', { templateUrl: 'partials/aiken.html', title: 'Aiken' }).when('/blog', { templateUrl: 'partials/blog.html', title: 'Blog', controller: 'BlogCtrl' }).when('/coding', { templateUrl: 'partials/coding.html', title: 'Coding' }).when('/connect', { templateUrl: 'partials/connect.html', title: 'Connect' }).otherwise({ redirectTo: '/about_me' });
     }]);
 
+// CORS config
+homepageApp.config([
+    '$httpProvider',
+    function ($httpProvider) {
+        // ...
+        // delete header from client:
+        // http://stackoverflow.com/questions/17289195/angularjs-post-data-to-external-rest-api
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }]);
+
 // set the page title on route change
 homepageApp.run(function ($rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current) {
