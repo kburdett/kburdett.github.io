870131292989196195d4c3b71d84fd8e4e1f82ef
Kevin Burdett <kevin@kevinburdett.com>
2014-09-23 17:40:22 -0400

    Upgrade all libraries and typings and move domReady to CDN

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@
 ///<reference path='../typings/requirejs/require.d.ts' />
 require.config({
     paths: {
-        'jquery': '//code.jquery.com/jquery-2.1.0.min',
-        'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min',
-        'angular-route': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min',
-        'angular-sanitize': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-sanitize.min',
-        'angular-resource': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-resource.min',
+        'jquery': 'https://code.jquery.com/jquery-2.1.1.min',
+        'angular': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min',
+        'angular-route': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.min',
+        'angular-sanitize': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-sanitize.min',
+        'angular-resource': 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-resource.min',
         'angulartics': 'dist/angulartics.min',
         'angulartics-ga': 'dist/angulartics-ga.min',
-        'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min'
+        'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min',
+        'domReady': 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min'
     },
     shim: {
         'angular': {
             exports: 'angular',
-            deps: ['jquery', 'dist/domReady!']
+            deps: ['jquery', 'domReady!']
         },
         'angular-route': ['angular'],
         'angular-resource': ['angular'],
@@ -26,7 +27,7 @@ require.config({
 });
 
 // startup the application
-require(['angular', 'dist/domReady!', 'app', 'bootstrap'], function (angular, document) {
+require(['angular', 'domReady!', 'app', 'bootstrap'], function (angular, document) {
     // bootstrap the document, since we are loading asynchronously
     angular.bootstrap(document, ['homepage.app']);
 });
